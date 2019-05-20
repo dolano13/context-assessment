9d83584a7fbf1034b344cee3bbc75a05889d652a
Daniela Olano <danielaolano96@gmail.com>
2019-05-20 16:25:17 -0400

    done

--- src/App.js
+++ src/App.js
@@ -34,56 +34,78 @@ class A extends React.Component {
   // }
   render() {
     return (
-      <div className="component-A">
       <MyContext.Consumer>
-        <h1>Component A</h1>
-        {(context)=>(
-          The state in the component A is {context.state.x} <br />
-          context.handleXClick will set the state to a new value => {"context.setState({x: context.state.x + 1})"} 
-          <B x={context.state.x} onXClick={context.handleXClick} />
-          <D x={context.state.x} onXClick={context.handleXClick} />
-       
-        )}
+        {(context)=>{
+          return(
+            <div className="component-A">
+            <h1>Component A</h1>
+            The state in the component A is {context.state.x} <br />
+            context.handleXClick will set the state to a new value => {"this.setState({x: this.state.x + 1})"} 
+            <B/>
+            <D/>
+            </div>
+          )
+        }}
       </MyContext.Consumer>
-      </div>
     );
   }
 }
 class B extends React.Component {
   render() {
     return (
-      <div className="component-B">
-        <h1>Component B</h1>
-        this.props.x = {context.state.x} <br />
-        this.props.onXClick => (method from component A) <br />
-        <C x={context.state.x} onXClick={context.onXClick} />
-      </div>
+      <MyContext.Consumer>
+        {(context)=>{
+          return(
+            <div className="component-B">
+            <h1>Component B</h1>
+            this.props.x = {context.state.x} <br />
+            this.props.onXClick => (method from component A) <br />
+            <C/>
+          </div>
+          )
+        }}
+      </MyContext.Consumer>
     );
   }
 }
 class C extends React.Component {
   render() {
     return (
-      <div className="component-C">
-        <h1>Component C</h1>
-        this.props.x = {context.state.x} <br />
-        this.props.onXClick => (method passed from component B) <br />
-        <button onClick={context.onXClick}>{context.state.x}</button> onClick =>
-        this.props.onXClick
-      </div>
+      <MyContext.Consumer>
+        {(context)=>{
+          return(
+            <div className="component-C">
+            <h1>Component C</h1>
+            this.props.x = {context.state.x} <br />
+            this.props.onXClick => (method passed from component B) <br />
+            <button onClick={context.handleXClick}>{context.state.x}</button> onClick =>
+            this.props.onXClick
+          </div>
+          )
+        }}
+      
+
+      </MyContext.Consumer>
     );
   }
 }
 class D extends React.Component {
   render() {
     return (
-      <div className="component-D">
-        <h1>Component D</h1>
-        this.props.x = {context.state.x} <br />
-        this.props.onXClick => (method from component A) <br />
-        <button onClick={context.onXClick}>{context.state.x}</button> onClick =>
-        this.props.onXClick
-      </div>
+      <MyContext.Consumer>
+        {(context)=>{
+          return(
+          <div className="component-D">
+            <h1>Component D</h1>
+            this.props.x = {context.state.x} <br />
+            this.props.onXClick => (method from component A) <br />
+            <button onClick={context.handleXClick}>{context.state.x}</button> onClick =>
+            this.props.onXClick
+          </div>
+
+          )
+          }}
+      </MyContext.Consumer>
     );
   }
 }
@@ -91,10 +113,11 @@ class D extends React.Component {
 
 function App() {
   return (
+    <MyProvider>
     <div className="App">
-      <MyProvider/>
       <A />
     </div>
+    </MyProvider>
   );
 }
 
